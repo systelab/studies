39d168d3e66bca132ac9b0acf6e46ed67e915c09
alfonsserra <alfonsoserra@gmail.com>
2018-12-17 20:18:37 +0100

    Use lombok

--- src/main/java/com/systelab/studies/model/study/StudyResultId.java
+++ src/main/java/com/systelab/studies/model/study/StudyResultId.java
@@ -1,11 +1,18 @@
 package com.systelab.studies.model.study;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 import java.util.UUID;
 
 @Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudyResultId implements Serializable {
 
     @Column(name = "fk_study")
@@ -13,49 +20,4 @@ public class StudyResultId implements Serializable {
 
     @Column(name = "fk_result")
     protected Long resultId;
-
-    public StudyResultId() {
-    }
-
-    public StudyResultId(UUID studyId, Long resultId) {
-        this.studyId = studyId;
-        this.resultId = resultId;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result
-                + ((studyId == null) ? 0 : studyId.hashCode());
-        result = prime * result
-                + ((resultId == null) ? 0 : resultId.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-
-        StudyResultId other = (StudyResultId) obj;
-
-        if (studyId == null) {
-            if (other.studyId != null)
-                return false;
-        } else if (!studyId.equals(other.studyId))
-            return false;
-
-        if (resultId == null) {
-            if (other.resultId != null)
-                return false;
-        } else if (!resultId.equals(other.resultId))
-            return false;
-
-        return true;
-    }
 }
